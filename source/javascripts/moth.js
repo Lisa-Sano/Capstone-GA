844b02ef3223adacb35bc7e693642207b50de705
Lisa Rolczynski <lisa.sano@gmail.com>
2016-08-09 11:42:59 -0700

    remove magic length number from the lpad function call in randChromosome. add comment to clarify what lpad is doing

--- source/javascripts/moth.js
+++ source/javascripts/moth.js
@@ -1,13 +1,14 @@
 var Moth = function(obj={}) {
   this.chrom_length = 8,
-  this.chromosome = obj.chrom || randChromosome()
+  this.chromosome = obj.chrom || randChromosome(this.chrom_length)
 
-  function randChromosome() {
+  function randChromosome(length) {
     var num = Math.floor(Math.random() * 256) + 1 ;
-    return lpad(num.toString(2), "0", 8);
+    return lpad(num.toString(2), "0", length);
   }
 }
 
+// add 0's to the left so that each chromosome is the same chromosome length
 function lpad(string, pad, length) {
     var str = string;
     while (str.length < length)
