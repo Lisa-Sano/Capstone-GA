9d2f65c8c629cc02f6bf295d5de7b7e8cf005883
Lisa Rolczynski <lisa.sano@gmail.com>
2016-08-10 14:54:22 -0700

    add initChart and resetChart functionality (a lot of original updateChart code moved to initChart. updateChart only updates ending population bars). add start and reset buttons that control the simulation.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,37 +3,52 @@ var Simulate = require('./simulate');
 var frequency = require('./frequency');
 var Chart = require('./barChart');
 
-var chart = new Chart();
 
-var pop = new Population();
-var starting = frequency(pop.chrom_vals);
-var num_gens = 100;
-var counter = 0;
-var ending;
+$(document).ready(function() {
+  var chart = new Chart();
+  var pop = new Population();
+  var starting = frequency(pop.chrom_vals);
 
-for (let i = 0; i < num_gens; i += 1) {
-  
-  // the most changes will occur at generations < 15, so update
-  // chart every generation until 15, then every 5 until 30, then
-  // every 10 until the end
-  if (i < 15) {
-    var n = 1;
-  } else if (i < 30) {
-    var n = 5;
-    i += 4;
-  } else {
-    var n = 10;
-    i += 9;
-  }
+  chart.initChart(starting, (Math.round(pop.env/12.75)*5));
 
-  // update chart every 250ms
-  setTimeout(function() {
-    pop = Simulate(n, pop);
-    ending = frequency(pop.chrom_vals);
-    chart.updateChart(starting, ending, (Math.round(pop.env/12.75)) * 5);
-    console.log("number: " + i)},
-    (counter * 250));
+  $("#start").click(function() {
+    var num_gens = 100;
+    var counter = 0;
+    var ending;
 
-  counter++;
-}
+    for (let i = 0; i < num_gens; i += 1) {
+      
+      // the most changes will occur at generations < 15, so update
+      // chart every generation until 15, then every 5 until 30, then
+      // every 10 until the end
+      if (i < 15) {
+        var n = 1;
+      } else if (i < 30) {
+        var n = 5;
+        i += 4;
+      } else {
+        var n = 10;
+        i += 9;
+      }
 
+      // update chart every 250ms
+      setTimeout(function() {
+        pop = Simulate(n, pop);
+        ending = frequency(pop.chrom_vals);
+        chart.updateChart(starting, ending);
+        console.log("number: " + i)},
+        (counter * 250));
+
+      counter++;
+    }
+  });
+
+  $("#reset").click(function() {
+    console.log("RESET!!!!!");
+    pop = new Population();
+    starting = frequency(pop.chrom_vals);
+
+    $(".gchart").remove();
+    chart.resetChart(starting, (Math.round(pop.env/12.75)*5));
+  });
+});
--- source/javascripts/barChart.js
+++ source/javascripts/barChart.js
@@ -2,7 +2,9 @@ var Chart = function() {
     this.margin = {top: 30, right: 50, bottom: 50, left: 50},
     this.width = 500 - this.margin.left - this.margin.right,
     this.height = 370 - this.margin.top - this.margin.bottom,
-    this.updateChart = function(starting_data, ending_data, environ) {
+    this.initChart = function(starting_data, environ) {     
+
+    // INITIALIZE CHART and LEGEND 
       d3.select(".chart")
         .attr("width", this.width + this.margin.left + this.margin.right)
         .attr("height", this.height + this.margin.top + this.margin.bottom);
@@ -10,36 +12,34 @@ var Chart = function() {
       d3.select(".legend")
         .attr("transform", "translate(" + ((this.width - this.margin.left) / 2) + "," + (this.height + this.margin.bottom + 10) + ")");
 
+      // INITIALIZE GLOBAL TAG
       var svg = d3.select(".chart").selectAll(".gchart").data([starting_data]);
         
       svg.enter().append("g")
         .attr("class", "gchart")
         .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
-
+      
+      // SET ORDINAL X AXIS AND LINEAR Y AXIS RANGES
       var height = this.height;
       var width = this.width;
-
       var x = d3.scaleBand()
         .rangeRound([0, width]);
-
       var y = d3.scaleLinear()
+        .domain([0,1])
         .range([height, 0]);
 
-      var y1 = d3.scaleLinear()
-        .range([height, 0]);
-
+      // REFERENCE AXES
       var xAxis = d3.axisBottom(x);
       var yAxis = d3.axisLeft(y).ticks(10, "%");
 
+      // SET X DOMAIN BASED ON SATURATION VALUES
       x.domain(starting_data.map(function(d) { return d.percent; }));
-      // y.domain([0, (d3.max(ending_data, function(d) { return d.frequency; }) + .03)]);
-      y.domain([0,1]);
 
+      // SET BAR WIDTH BASED ON DATA SET
       var barWidth = (width - 100) / starting_data.length;
-
-      var axisL = d3.select(".gchart").selectAll(".axisLeft").data([ending_data]);
       
-      axisL.enter().append("g")
+      // CREATE LEFT Y AXIS
+      d3.select(".gchart").append("g")
           .attr("class", "y axis axisLeft")
           .attr("transform", "translate(0,0)")
           .call(yAxis)
@@ -50,20 +50,28 @@ var Chart = function() {
           .style("text-anchor", "end")
           .text("Frequency");
 
-      svg.select('g.y.axis')
-        .call(yAxis);
+      // SHOW COLOR GRADIENT ON THE X AXIS
+      var colorAxis = d3.select(".gchart").selectAll(".saturation").data(starting_data);
 
-      var envL = d3.select(".gchart").selectAll(".envLine").data([starting_data]);
-        
-      envL.enter().append("rect")
+      colorAxis.enter().append("rect")
+          .attr("class", function(d) { return "saturation percent" + d.percent})
+          .attr("x", function(d) { return x(d.percent) - 2.5; })
+          .attr("y", height)
+          .attr("height", "20")
+          .attr("width", "19");
+
+      // SHOW ENVIRONMENT COLOR
+      d3.select(".gchart").append("rect")
           .attr("class", "envLine")
           .attr("filter", "url(#f1)")
           .attr("x", x(environ))
           .attr("y", "0")
-          .attr("height", height)
+          .attr("height", height - 2)
           .attr("width", "20");
 
-      var bar1 = svg.selectAll(".bar1").data(starting_data);
+      // STARTING POPULATION DATA
+      // bind data
+      var bar1 = d3.select(".gchart").selectAll(".bar1").data(starting_data);
 
       bar1.enter().append("rect")
           .attr("class", "bar bar1")
@@ -73,24 +81,56 @@ var Chart = function() {
           .attr("y", function(d) { return y(d.frequency); })
           .attr("height", function(d) { return height - y(d.frequency); });
 
-      var bar2 = svg.selectAll(".bar2").data(ending_data);
+      var bar2 = d3.select(".gchart").selectAll(".bar2").data(starting_data);
 
       bar2.enter().append("rect")
           .attr("class", "bar bar2")
           .attr("width", barWidth/2)
-        .merge(bar2)
+          .attr("x", function(d) { return x(d.percent) + (barWidth/2) + 1; })
+        .merge(bar1)
+          .attr("y", function(d) { return y(d.frequency); })
+          .attr("height", function(d) { return height - y(d.frequency); });
+    },
+    this.updateChart = function(starting_data, ending_data) {
+      var svg = d3.select(".gchart");
+        
+      var height = this.height;
+      var width = this.width;
+
+      var x = d3.scaleBand()
+        .rangeRound([0, width]);
+
+      var y = d3.scaleLinear()
+        .range([height, 0]);
+
+      x.domain(ending_data.map(function(d) { return d.percent; }));
+      // y.domain([0, (d3.max(ending_data, function(d) { return d.frequency; }) + .03)]);
+      y.domain([0,1]);
+
+      var barWidth = (width - 100) / ending_data.length;
+
+      var t = d3.transition()
+          .duration(250)
+          .ease(d3.easeLinear);
+
+      // bind data
+      var bar2 = svg.selectAll(".bar2").data(ending_data);
+
+      // enter
+      bar2.enter().append("rect")
+          .attr("class", "bar bar2")
+          .attr("width", barWidth/2);
+
+
+      // update
+      bar2.merge(bar2).transition(t)
           .attr("x", function(d) { return x(d.percent) + (barWidth/2) + 1; })
           .attr("y", function(d) { return y(d.frequency); })
           .attr("height", function(d) { return height - y(d.frequency); });
-
-      var colorAxis = svg.selectAll(".saturation").data(starting_data);
-
-      colorAxis.enter().append("rect")
-          .attr("class", function(d) { return "saturation percent" + d.percent})
-          .attr("x", function(d) { return x(d.percent) - 2.5; })
-          .attr("y", height)
-          .attr("height", "20")
-          .attr("width", "19");
+    },
+    this.resetChart = function(starting_data, environ) {
+      d3.select(".gchart").remove();
+      this.initChart(starting_data, environ);
     }
 }
 
