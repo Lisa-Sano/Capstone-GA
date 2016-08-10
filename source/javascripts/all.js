ab8c2e088167b7b590597902c119ad2926fb79e3
Lisa Rolczynski <lisa.sano@gmail.com>
2016-08-10 10:10:53 -0700

    update chart every gen if under 15, then every 5 if under 30, then every 10

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -5,17 +5,31 @@ var Chart = require('./barChart');
 
 
 var chart = new Chart();
-chart.chart;
 
 var pop = new Population();
 var starting = frequency(pop.chrom_vals);
 var num_gens = 100;
+var counter = 0;
 
-for (let i = 0; i < num_gens; i += 5) {
+for (let i = 0; i < num_gens; i += 1) {
+  
+  if (i < 15) {
+    var n = 1;
+  } else if (i < 30) {
+    var n = 5;
+    i += 4;
+  } else {
+    var n = 10;
+    i += 9;
+  }
+
   setTimeout(function() {
-
-    pop = Simulate(5, pop);
+    pop = Simulate(n, pop);
     var ending = frequency(pop.chrom_vals);
-    chart.updateChart(starting, ending, (Math.round(pop.env/12.75)) * 5)
-    console.log("number " + i)}, (i * 50));
+    chart.updateChart(starting, ending, (Math.round(pop.env/12.75)) * 5);
+    if (i >= num_gens-1) { console.log("DONE!")}},
+    (counter * 200));
+
+  counter++;
 }
+
