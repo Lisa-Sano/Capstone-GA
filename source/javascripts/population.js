0fdb7cd52dd3e2a53442dc587645066bc7a5fbd8
Lisa Rolczynski <lisa.sano@gmail.com>
2016-08-11 16:18:44 -0700

    moved non-property functions outside of Population constructor

--- source/javascripts/population.js
+++ source/javascripts/population.js
@@ -27,89 +27,90 @@ var Population = function(pop={}) {
     let new_moth = new Moth({chrom: new_chrom});
     return new_moth;
   }
-
-  // create an array of chromosome values for the entire population
-  function getChromVals(population) {
-    let vals = [];
-    population.forEach(function(m) {
-      vals.push(m.value);
-    })
-    return vals;
-  }
-
-  // if population not initialized with an array of moths, generate a new population
-  function generateMoths(num) {
-    let pop = [];
-    for (let i = 0; i < num; i++) {
-      let m = new Moth();
-      pop.push(m);
-    }
-
-    return pop;
-  }
-
-  // calculate the fitness (closer value of chromosome compared to environment value = higher score)
-  function evalFitness(pop, max, env) {
-    let f = [];
-    pop.forEach(function(m) {
-      f.push(max - (Math.abs(env - m.value) * 0.2));
-    });
-
-    return f;
-  }
-
-  // weighted probabilities based on fitness score
-  // higher fitness scores have higher chance of being selected
-  function probabilities(fitness) {
-    let fit_sum = fitness.reduce(function(acc, cur) {
-      return acc + cur;
-    }, 0);
-    let prob_sum = 0.0;
-    let probs = [];
-    let counter = 0;
-
-    fitness.forEach(function(f) {
-      probs[counter] = prob_sum + (f / fit_sum);
-      prob_sum = probs[counter];
-      counter++;
-    });
-
-    return probs;
-  }
-
-  // returns a Moth, sampled using the weighted probabilities
-  function weightedSample(probs, population) {
-    let r = Math.random();
-
-    for (let i = 0; i < probs.length; i++) {
-      if (r < probs[i]) {
-        return population[i];
-      }
-    };
-
-    return population[probs.length - 1];
-  }
-
-  // mutates 0 to 1, or 1 to 0 (goes through every index and mutates
-  // if willMutate() returns true)
-  function mutateChromosome(chrom) {
-    let chrom_array = chrom.split('');
-    let counter = 0;
-
-    chrom_array.forEach(function(n) {
-      if (willMutate()) {
-        chrom_array[counter] = [1, 0][parseInt(n)];
-      }
-      counter++;
-    });
-
-    return chrom_array.join('');
-  }
-
-  // determine whether a single binary value will mutate
-  function willMutate(rate=0.0005) {
-    return Math.floor(Math.random() * (1/rate)) === 0;
-  }
 }
+
+// create an array of chromosome values for the entire population
+function getChromVals(population) {
+  let vals = [];
+  population.forEach(function(m) {
+    vals.push(m.value);
+  })
+  return vals;
+}
+
+// if population not initialized with an array of moths, generate a new population
+function generateMoths(num) {
+  let pop = [];
+  for (let i = 0; i < num; i++) {
+    let m = new Moth();
+    pop.push(m);
+  }
+
+  return pop;
+}
+
+// calculate the fitness (closer value of chromosome compared to environment value = higher score)
+function evalFitness(pop, max, env) {
+  let f = [];
+  pop.forEach(function(m) {
+    f.push(max - (Math.abs(env - m.value) * 0.2));
+  });
+
+  return f;
+}
+
+// weighted probabilities based on fitness score
+// higher fitness scores have higher chance of being selected
+function probabilities(fitness) {
+  let fit_sum = fitness.reduce(function(acc, cur) {
+    return acc + cur;
+  }, 0);
+  let prob_sum = 0.0;
+  let probs = [];
+  let counter = 0;
+
+  fitness.forEach(function(f) {
+    probs[counter] = prob_sum + (f / fit_sum);
+    prob_sum = probs[counter];
+    counter++;
+  });
+
+  return probs;
+}
+
+// returns a Moth, sampled using the weighted probabilities
+function weightedSample(probs, population) {
+  let r = Math.random();
+
+  for (let i = 0; i < probs.length; i++) {
+    if (r < probs[i]) {
+      return population[i];
+    }
+  };
+
+  return population[probs.length - 1];
+}
+
+// mutates 0 to 1, or 1 to 0 (goes through every index and mutates
+// if willMutate() returns true)
+function mutateChromosome(chrom) {
+  let chrom_array = chrom.split('');
+  let counter = 0;
+
+  chrom_array.forEach(function(n) {
+    if (willMutate()) {
+      chrom_array[counter] = [1, 0][parseInt(n)];
+    }
+    counter++;
+  });
+
+  return chrom_array.join('');
+}
+
+// determine whether a single binary value will mutate
+function willMutate(rate=0.0005) {
+  return Math.floor(Math.random() * (1/rate)) === 0;
+}
+
 
 module.exports = Population;
