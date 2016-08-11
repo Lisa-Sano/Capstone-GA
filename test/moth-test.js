232754c09a08c1391f7439d17f09848991a7942d
Lisa Rolczynski <lisa.sano@gmail.com>
2016-08-11 14:46:30 -0700

    add more unit tests for Moth object

--- test/moth-test.js
+++ test/moth-test.js
@@ -2,10 +2,12 @@ var assert = require('assert');
 var Moth = require('../source/javascripts/moth');
 
 var m;
+var m2;
 
 describe('Moth', function() {
   before(function(){
     m = new Moth();
+    m2 = new Moth({chrom: '00001111'});
   });
 
   describe('chromosome property', function() {
@@ -25,6 +27,20 @@ describe('Moth', function() {
 
     it('should be the same as the length of the chromosome', function() {
       assert.equal(m.chrom_length, m.chromosome.length);
-    })
-  })
+    });
+  });
+
+  describe('value property', function() {
+    it('should equal the number equivalent of the binary chromosome', function() {
+      assert.equal(15, parseInt(m2.chromosome, 2));
+    });
+  });
+
+  describe('initializing a new moth', function() {
+    it('can be initialized with a specific chromosome binary number', function() {
+      assert.equal('00001111', m2.chromosome);
+      assert.equal(15, parseInt(m2.chromosome, 2));
+      assert.equal(8, m.chrom_length);
+    });
+  });
 });
