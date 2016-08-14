4c8f2bbc09e1dbeec3facd89ddfe2f094a8ddc63
Lisa Rolczynski <lisa.sano@gmail.com>
2016-08-13 23:44:03 -0700

    add radio buttons so the user can choose either a uniform population (all white on a black background) or random (random moth colors and environment)

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -6,37 +6,43 @@ var Graphic = require('./main');
 
 
 $(document).ready(function() {
-  var chart = new Chart();
-  var pop = new Population();
+  var pop = getPopulation();
   var starting = frequency(pop.chrom_vals);
   var graphic = new Graphic();
   var environ = (Math.round(pop.env/12.75)*5);
+  var chart = new Chart();
+
+
 
   chart.initChart(starting, environ);
 
   graphic.drawGraphic(pop.chrom_vals, pop.env);
 
   $("#start").click(function() {
-    var num_gens = 200;
+    var num_gens = 500;
     var counter = 0;
     var ending;
+    var n;
 
     for (let i = 0; i < num_gens; i += 1) {
       
       // the most changes will occur at generations < 15, so update
       // chart every generation until 15, then every 5 until 30, then
       // every 10 until the end
-      if (i < 15) {
-        var n = 1;
+      if (i < 10) {
+        n = 1;
       } else if (i < 30) {
-        var n = 5;
+        n = 5;
         i += 4;
       } else if (i < 100) {
-        var n = 10;
+        n = 10;
         i += 9;
-      } else {
-        var n = 20;
+      } else if (i < 200) {
+        n = 20;
         i += 19;
+      } else {
+        n = 50;
+        i += 49;
       }
 
       // update chart every 250ms
@@ -53,9 +59,10 @@ $(document).ready(function() {
     }
   });
 
-  $("#reset").click(function() {
+  $("#reset, .radio").click(function() {
     console.log("RESET!!!!!");
-    pop = new Population();
+
+    pop = getPopulation();
     starting = frequency(pop.chrom_vals);
 
     $(".gchart").remove();
@@ -67,3 +74,17 @@ $(document).ready(function() {
     $(".gen").html("1")
   });
 });
+
+function getPopulation() {
+  var uniform;
+  var properties = {};
+
+  if ($('input[id=uniform]:checked').length > 0) {
+    uniform = true;
+    properties.env = 235;
+  } else {
+    uniform = false;
+  }
+
+  return new Population(properties, uniform);
+}
