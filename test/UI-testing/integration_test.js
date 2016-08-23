10e63cd098af396c73cdfe840a4e047c49a847b0
Lisa Rolczynski <lisa.sano@gmail.com>
2016-08-23 14:07:52 -0700

    add tests to check that bar chart and 3d plot are hidden or visible at the right time

--- test/UI-testing/integration_test.js
+++ test/UI-testing/integration_test.js
@@ -1,6 +1,6 @@
 var assert = require('assert'),     
     fs = require('fs'),     
-    test = require('selenium-webdriver/testing'),     
+    // test = require('selenium-webdriver/testing'),     
     webdriver = require('selenium-webdriver');
 
 require('chai').should();
@@ -13,34 +13,80 @@ describe('Evolution in Color', function() {
     driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
   });
 
-  // test.it('Page should return success', function(){     
-  //   driver.get('http://localhost:4567'); 
-  //   driver.findElement(webdriver.By.id('color').click());
-  //   // driver.findElement(webdriver.By.name('name')).sendKeys('Glenn Taylor');
-  //   // driver.findElement(webdriver.By.name('email')).sendKeys('glenn@glenntaylor.co.uk');
-  //   // driver.findElement(webdriver.By.name('message')).sendKeys('This is a test message!');
-  //   // driver.findElement(webdriver.By.className('contact-form')).submit();
-  //   driver.wait(function() {
-  //       return driver.isElementPresent(webdriver.By.className('success-flash'));
-  //   }, 3000);
-  // });
+  it('should return success', function() {
+    driver.get('http://localhost:4567');
+  });
 
-  describe('My site', function() {
-    it('should return success', function() {
-      driver.get('http://localhost:4567');
-        // .then(done()));
+  it('should have a canvas element', function(done) {
+    driver.findElement(webdriver.By.tagName('canvas'))
+    .then(function(canvas) { done() });
+  });
+
+    it('should have 1 (radio) button with id color', function(done) {
+      driver.findElements(webdriver.By.id("color"))
+        .then(function(color) { 
+          color.length.should.equal(1);
+          done();
+        });
     });
 
-    it('should have the title "Evolution in Color"', function() {
-      driver.getTitle().then(function(title) { title.should.equal('Evolution in Color')});
+    it('should have 1 (radio) button with id black-white', function(done) {
+      driver.findElements(webdriver.By.id("black-white"))
+        .then(function(color) { 
+          color.length.should.equal(1);
+          done();
+        });
     });
 
-    it('should have a canvas element', function(done) {
-      driver.findElement(webdriver.By.tagName('canvas'))
-      .then(function() { done(); });
+  describe('Chart displays', function() {
+    it('bar chart should be hidden when the color radio button is active', function(done) {
+      driver.findElement(webdriver.By.id("color")).click();
+
+      driver.findElement(webdriver.By.id('chart')).getAttribute("style")
+        .then(function(style) {
+          style.should.equal("display: none;");
+          done();
+        });
+    });
+
+    it('3D scatter plot should be visible when the color radio button is active', function(done) {
+      driver.findElement(webdriver.By.id("color")).click();
+
+      driver.findElement(webdriver.By.id('container3d')).getAttribute("style")
+        .then(function(style) {
+          style.should.equal("display: initial;");
+          done();
+        })
+    });
+
+    it('bar chart should be visible when the black-white radio button is active', function(done) {
+      driver.findElement(webdriver.By.id("black-white")).click();
+
+      driver.findElement(webdriver.By.id('chart')).getAttribute("style")
+        .then(function(style) {
+          style.should.equal("display: initial;");
+          done();
+        });
+    });
+
+    it('3D scatter plot should be hidden when the black-white radio button is active', function(done) {
+      driver.findElement(webdriver.By.id("black-white")).click();
+
+      driver.findElement(webdriver.By.id('container3d')).getAttribute("style")
+        .then(function(style) {
+          style.should.equal("display: none;");
+          done();
+        })
     });
   });
 
+  it('should have the title "Evolution in Color"', function(done) {
+    driver.getTitle()
+      .then(function(title) {
+        title.should.equal('Evolution in Color');
+        done();
+    });
+  });
 
   after(function() {
     driver.quit();
