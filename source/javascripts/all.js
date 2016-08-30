d0340d32b6cfc50d95b200bbf1d6edcaf29583be
Lisa Rolczynski <lisa.sano@gmail.com>
2016-08-30 10:35:33 -0700

    make button border transparent when button is selected

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -349,13 +349,13 @@ $(document).ready(function() {
 
   function unselectButtons() {
     $('button').css("background-color", "");
-    // $('button').css("border", "");
+    $('button').css("border", "");
   }
 
   function selectButton(this_button) {
     $(this_button).blur();
     $(this_button).css("background-color", "#aaa");
-    // $(this_button).css("border", "transparent");
+    $(this_button).css("border", "transparent");
   }
 });
 
